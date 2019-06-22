bda8ce3e657f7dc9823927e37fde5870ef652905
hesara111 <45358754+hesara111@users.noreply.github.com>
2019-06-22 22:43:32 +0300

    222

---
+++ Project.Core/Stuff/CObjective.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Core.Stuff
+{
+    public class CObjective
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string StatusName { get; set; }
+
+        public int StatusID { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
